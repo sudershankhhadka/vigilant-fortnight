f11649b4fa37783cf511b49d2f429113208afd0e
Sudershan Khadka <106295087+sudershankhhadka@users.noreply.github.com>
2024-02-29 20:31:36 +0545

    connections shown through controller

--- src/main/java/com/bway/SpringCoreDemo/controller/loginController.java
+++ src/main/java/com/bway/SpringCoreDemo/controller/loginController.java
@@ -1,7 +1,12 @@
 package com.bway.SpringCoreDemo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.bway.SpringCoreDemo.model.User;
 @Controller
 public class loginController {
 	@GetMapping("/login")
@@ -9,4 +14,20 @@ public class loginController {
 		return "loginForm";
 		
 	}
+	@PostMapping("/login")
+	public String postLogin(@ModelAttribute User u,Model model) {
+		
+		if(u.getUsername().equals("hari") && u.getPassword().equals("123")) {
+			model.addAttribute("uname",u.getUsername());
+			return "Home";
+		}
+		
+		
+		model.addAttribute("message","user not found");
+		return "loginForm";
+			
+	}
 }
+
+
+
--- src/main/java/com/bway/SpringCoreDemo/controller/signupController.java
+++ src/main/java/com/bway/SpringCoreDemo/controller/signupController.java
@@ -1,11 +1,28 @@
 package com.bway.SpringCoreDemo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.bway.SpringCoreDemo.model.User;
 @Controller
 public class signupController {
 	@GetMapping("/signup")
 	public String signup() {
 		return "signUp";
+	}
+	
+	
+	@PostMapping("/signup")
+	public String postSignup(@ModelAttribute User u,Model model ) {
+		model.addAttribute("fname",u.getFname());
+		model.addAttribute("lname" ,u.getLname());
+		model.addAttribute("username",u.getUsername());
+		model.addAttribute("email",u.getEmail());
+		model.addAttribute("password",u.getPassword());
+		return "profile";
+		
 	}
 }
